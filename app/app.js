07c766f38490d412706a61b6fc6044dc3cb4c553
sharonrussell <sharon.russell@comparethemarket.com>
2016-04-13 10:47:39 +0100

    Setup the jade view structure

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
 var express = require('express')
   , app = express()
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-  res.send('Site under construction!');
+  res.render('index');
 });
 
 app.listen(3000, function() {
